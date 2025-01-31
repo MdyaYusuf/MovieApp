3fc4c9cb2d3ca95463e76bdb6d9457c0acd64718
MdyaYusuf <myusufunpostakutusu@gmail.com>
2025-01-31 11:13:21 +0300

    Fetched data and added more movie details.

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,136 @@
-import { useState } from "react";
-
-const movie_list = [
-  {
-    Id: "769",
-    Title: "GoodFellas",
-    Year: "1990",
-    Poster: "/img/goodfellas.jpg"
-  },
-  {
-    Id: "120",
-    Title: "Lord of the Rings",
-    Year: "2001",
-    Poster: "/img/lordoftherings.jpg"
-  },
-  {
-    Id: "27205",
-    Title: "Inception",
-    Year: "2010",
-    Poster: "/img/inception.jpg"
-  },
-  {
-    Id: "105",
-    Title: "Back to the Future",
-    Year: "1990",
-    Poster: "/img/backtothefuture.jpg"
-  }
-];
-
-const selected_movie_list = [
-  {
-    Id: "27205",
-    Title: "Inception",
-    Year: "2010",
-    Poster: "/img/inception.jpg",
-    Duration: 120,
-    Rating: 8.4
-  },
-  {
-    Id: "105",
-    Title: "Back to the Future",
-    Year: "1990",
-    Poster: "/img/backtothefuture.jpg",
-    Duration: 110,
-    Rating: 7.6
-  }
-];
+import { useEffect, useState } from "react";
+import StarRating from './StarRating.jsx';
 
 const getAverage = (array) => array.reduce((sum, value) => sum + value, 0) / array.length;
 
 export default function App() {
+  const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [selectedMovies, setSelectedMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  function handleSelectedMovie(id) {
+    setSelectedMovie(selectedMovie => id === selectedMovie ? null : id);
+  }
+
+  function handleUnselectMovie(id) {
+    setSelectedMovie(null);
+  }
+
+  function handleAddToList(movie) {
+    setSelectedMovies(selectedMovies => [...selectedMovies, movie]);
+    handleUnselectMovie();
+  }
+
+  function handleDeleteFromList(id) {
+    setSelectedMovies(selectedMovies => selectedMovies.filter(m => m.id !== id));
+  }
+
+  useEffect(function () {
+    const controller = new AbortController();
+    const signal = controller.signal;
+    
+    async function getMovies() {
+      try {
+        setLoading(true);
+        setError("");
+
+        const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${query}`, { signal: signal });
+
+        if (res.ok) {
+          throw new Error("Bilinmeyen bir hata oluştu.");
+        }
+
+        const data = await res.json();
+
+        if (data.total_results === 0) {
+          throw new Error("Film bulunamadı.")
+        }
+        setMovies(data.results);
+      }
+      catch (err) {
+        if (err.name === "AbortError") {
+          console.log("aborted...");
+        } else {
+          setError(err.message);
+        }
+      }
+      setLoading(false);
+    }
+
+    if (query.length < 4) {
+      setMovies([]);
+      setError("");
+      return;
+    }
+
+    getMovies();
+
+    return () => {
+      controller.abort();
+    }
+  }, [query]);
+
+  const api_key = "insert some key here";
 
   return (
     <>
-      <Nav />
-      <Main />
+      <Nav>
+        <Logo />
+        <Search query={query} setQuery={setQuery} />
+        <NavSearchResult movies={movies} onSelectMovie={handleSelectedMovie} />
+      </Nav>
+      <Main>
+        <div className="row mt-2">
+          <div className="col-md-9">
+            <ListContainer>
+              {loading && <Loading />}
+              {!loading && !errorr && <MovieList movies={movies} onSelectMovie={handleSelectedMovie} selectedMovie={selectedMovie} />}
+              {error && <ErrorMessage message={error} />}
+            </ListContainer>
+          </div>
+          <div className="col-md-3">
+            <ListContainer>
+              {selectedMovie ? (
+                <MovieDetails
+                  selectedMovie={selectedMovie} onUnSelectMovie={handleUnselectMovie} onAddToList={handleAddToList}
+                  selectedMovies={selectedMovies}
+                />
+              ) : (
+                <>
+                  <MyMovieListSummary />
+                  <MyMovieList selectedMovies={selectedMovies} onDeleteFromList={handleDeleteFromList} />
+                </>
+              )}
+            </ListContainer>
+          </div>
+        </div>
+      </Main>
     </>
   );
 }
 
-function Nav() {
+function ErrorMessage({ message }) {
+  return (
+    <div className="alert alert-danger">{message}</div>
+  );
+}
+
+function Loading() {
+  return (
+    <div className="spinner-border" role="status">
+      <span className="sr-only">Loading...</span>
+    </div>
+  );
+}
+
+function Nav({ children }) {
   return (
     <nav className="bg-primary text-white p-2">
       <div className="container">
-        <div className="row align-items-center">
-          <Logo />
-          <Search />
-          <NavSearchResult />
-        </div>
+        <div className="row align-items-center">{children}</div>
       </div>
     </nav>
   );
@@ -82,41 +146,32 @@ function Logo() {
   );
 }
 
-function Search() {
+function Search({ query, setQuery }) {
 
   return (
     <div className="col-4">
-      <input type="text" className="form-control" placeholder="Film ara..." />
+      <input type="text" value={query} onChange={(e) => setQuery(e.target.value)} className="form-control" placeholder="Film ara..." />
     </div>
   );
 }
 
-function NavSearchResult() {
+function NavSearchResult({ movies }) {
 
   return (
     <div className="col-4 text-end">
-      <strong>5</strong> kayıt bulundu.
+      <strong>{movies.length}</strong> kayıt bulundu.
     </div>
   );
 }
 
-function Main() {
+function Main({ children }) {
 
   return (
-    <main className="container">
-      <div className="row mt-2">
-        <div className="col-md-9">
-          <MovieListContainer />
-        </div>
-        <div className="col-md-3">
-          <MyMovieListContainer />
-        </div>
-      </div>
-    </main>
+    <main className="container">{children}</main>
   );
 }
 
-function MovieListContainer() {
+function ListContainer({ children }) {
   const [isOpen, setIsOpen] = useState(true);
 
   return (
@@ -125,56 +180,117 @@ function MovieListContainer() {
         {isOpen ? (<i className="bi bi-chevron-up"></i>) : (<i className="bi bi-chevron-down"></i>)}
       </button>
       {
-        isOpen && <MovieList />
+        isOpen && children
       }
     </div>
   );
 }
 
-function MyMovieListContainer() {
-  const [selectedMovies, setSElectedMovies] = useState(selected_movie_list);
-  const [isSelectedOpen, setIsSelectedOpen] = useState(true);
-
-  return (
-    <div className="movie-list">
-      <button className="btn btn-sm btn-outline-primary mb-2" onClick={() => setIsSelectedOpen((val) => !val)}>
-        {isSelectedOpen ? (<i className="bi bi-chevron-up"></i>) : (<i className="bi bi-chevron-down"></i>)}
-      </button>
-      {
-        isSelectedOpen &&
-        <div>
-          <MyMovieListSummary />
-          <MyMovieList selectedMovies={selectedMovies} />
-        </div>
-      }
-    </div>
-  );
-}
-
-function MovieList() {
-  const [movies, setMovies] = useState(movie_list);
+function MovieList({ movies, onSelectMovie, selectedMovie }) {
 
   return (
     (
       <div className="row row-cols-1 row-cols-md-3 row-cols-xl-4 g-4">
         {
-          movies.map(movie => <Movie movie={movie} key={movie.Id} />)
+          movies.map(movie => <Movie movie={movie} key={movie.id} onSelectMovie={onSelectMovie} selectedMovie={selectedMovie} />)
         }
       </div>
     )
   );
 }
 
-function Movie({ movie }) {
+function MovieDetails({ selectedMovie, onUnSelectMovie, onAddToList, selectedMovies }) {
+
+  const [movie, setMovie] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [userRating, setUserRating] = useState("");
+
+  const isAddedToList = selectedMovies.map(m => m.id).includes(selectedMovie);
+  const selectedMovieUserRating = selectedMovies.find(m => m.id === selectedMovie)?.userRating;
+
+  function handleAddToList() {
+    const newMovie = {
+      ...movie,
+      userRating
+    }
+    onAddToList(newMovie);
+  }
+
+  useEffect(function () {
+    async function getMovieDetails() {
+      setLoading(true);
+      const response = await fetch(`https://api.themoviedb.org/3/movie/${selectedMovie}?api_key=${api_key}`);
+      const data = await res.json();
+      setMovie(data);
+      setLoading(false);
+    }
+    getMovieDetails();
+  }, [selectedMovie])
+
+  return (
+    <>
+      {loading ? <Loading /> :
+        <div className="border p-2 mb-3">
+          <div className="row">
+            <div className="col-4">
+              <img src={
+                movie.poster_path ?
+                  `https://media.themoviedb.org/t/p/w440_and_h660_face` + movie.poster_path : "/img/no-image.jpg"
+              } alt={movie.title} className="img-fluid rounded" />
+            </div>
+            <div className="col-8">
+              <h6>{movie.title}</h6>
+              <p>
+                <i className="bi bi-calendar2-date me-1"></i>
+                <span>{movie.release_date}</span>
+              </p>
+              <p>
+                <i className="bi bi-star-fill text-warning"></i>
+                <span>{movie.vote_avarage}</span>
+              </p>
+            </div>
+            <div className="col-12 border-top p-3 mt-3">
+              <p>{movie.overview}</p>
+              <p>{movie.genres?.map((genre) => (
+                <span key={genre.id} className="badge text-bg-primary me-1">
+                  {genre.name}
+                </span>
+              ))}
+              </p>
+              {
+                !isAddedToList ? (
+                  <>
+                    <div className="my-4">
+                      <StarRating maxRating={10} size={20} onRating={setUserRating} />
+                    </div>
+                    <button className="btn btn-primary me-1" onClick={() => handleAddToList(movie)}>Listeye Ekle</button>
+                  </>
+                ) : (
+                  <p>Film listenizde. Değerlendirmeniz: <i className="bi bi-stars text-warning me-2"></i> {selectedMovieUserRating}</p>
+                )
+              }
+              <button className="btn btn-danger" onClick={onUnSelectMovie}>Kapat</button>
+            </div>
+          </div>
+        </div>
+      }
+    </>
+  );
+}
+
+function Movie({ movie, onSelectMovie, selectedMovie }) {
   return (
     <div className="col mb-2">
-      <div className="card">
-        <img src={movie.Poster} alt={movie.Title} className="card-img-top" />
+      <div className={`card movie ${selectedMovie === movie.id ? "selected-movie" : ""}`} onClick={() => onSelectMovie(movie.id)}>
+        <img src={
+          movie.poster_path ?
+            `https://media.themoviedb.org/t/p/w440_and_h660_face` + movie.poster_path : "/img/no-image.jpg"
+        } alt={movie.title} className="card-img-top" />
         <div className="card-body">
-          <h6 className="card-title">{movie.Title}</h6>
+          <h6 className="card-title">{movie.title}</h6>
           <div>
             <i className="bi bi-calendar2-date me-1"></i>
-            <span>{movie.Year}</span>
+            <span>{movie.release_date}</span>
           </div>
         </div>
       </div>
@@ -183,8 +299,9 @@ function Movie({ movie }) {
 }
 
 function MyMovieListSummary() {
-  const avgRating = getAverage(selected_movie_list.map(m => m.Rating));
-  const avgDuration = getAverage(selected_movie_list.map(m => m.Duration));
+  const avgRating = getAverage(selectedMovies.map(m => m.vote_avarage));
+  const avgDuration = getAverage(selectedMovies.map(m => m.runtime));
+  const avgUserRating = getAverage(selectedMovies.map(m => m.userRating));
 
   return (
     <div className="card mb-2">
@@ -196,6 +313,10 @@ function MyMovieListSummary() {
             <span>{avgRating.toFixed(2)}</span>
           </p>
           <p>
+            <i className="bi bi-stars text-warning me-2"></i>
+            <span>{avgUserRating.toFixed(2)}</span>
+          </p>
+          <p>
             <i className="bi bi-hourglass text-warning me-2"></i>
             <span>{avgDuration}</span>
           </p>
@@ -205,33 +326,37 @@ function MyMovieListSummary() {
   );
 }
 
-function MyMovieList({ selectedMovies }) {
+function MyMovieList({ selectedMovies, onDeleteFromList }) {
 
   return (
-    selectedMovies.map(movie => <MyListMovie movie={movie} key={movie.Id} />)
+    selectedMovies.map(movie => <MyListMovie movie={movie} key={movie.id} onDeleteFromList={onDeleteFromList} />)
   );
 }
 
-function MyListMovie({movie }) {
+function MyListMovie({ movie, onDeleteFromList }) {
   return (
     <div className="card mb-2">
       <div className="row">
         <div className="col-4">
-          <img src={movie.Poster} alt={movie.Title} className="img-fluid rounded-start" />
+          <img src={
+            movie.poster_path ?
+              `https://media.themoviedb.org/t/p/w440_and_h660_face` + movie.poster_path : "/img/no-image.jpg"
+          } alt={movie.title} className="img-fluid rounded-start" />
         </div>
         <div className="col-8">
           <div className="card-body">
-            <h6 className="card-title">{movie.Title}</h6>
+            <h6 className="card-title">{movie.title}</h6>
             <div className="d-flex justify-content-between">
               <p>
                 <i className="bi bi-star-fill text-warning me-2"></i>
-                <span>{movie.Rating}</span>
+                <span>{movie.vote_avarage}</span>
               </p>
               <p>
                 <i className="bi bi-hourglass text-warning me-1"></i>
-                <span>{movie.Duration}</span>
+                <span>{movie.runtime}</span>
               </p>
             </div>
+            <button className="btn btn-danger" onClick={() => onDeleteFromList(movie.id)}>Sil</button>
           </div>
         </div>
       </div>
